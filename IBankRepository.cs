7012a8b8385c86b6ca2fb0c37113fe2253f7521d
Hemanth T R <trhemanth2@gmail.com>
2021-08-28 20:48:47 +0530

    adding interface and required models

--- IBankRepository.cs
+++ IBankRepository.cs
@@ -1,7 +1,37 @@
-﻿namespace BankingRepository
-{
-	interface IBankRepository
-	{
+﻿using BankingRepository.Model;
 
+namespace BankingRepository
+{
+	/// <summary>
+	/// Interface for bank repository.
+	/// </summary>
+	public interface IBankRepository
+	{
+		/// <summary>
+		/// Retrieves the list of banks.
+		/// </summary>
+		/// <returns></returns>
+		Task<IList<BankInformation>> FetchBanksAsync();
+
+		/// <summary>
+		/// Retrieves the list of banks.
+		/// </summary>
+		/// <param name="id">Unique identifier for bank.</param>
+		/// <returns>bank information.</returns>
+		Task<BankInformation> FetchBankAsync(Guid id);
+
+		/// <summary>
+		/// Creates a bank.
+		/// </summary>
+		/// <param name="bank"></param>
+		/// <returns>unique identifier.</returns>
+		Task<Guid> CreateBankAsync(Bank bank);
+
+		/// <summary>
+		/// Updates bank's status.
+		/// </summary>
+		/// <param name="id">bank id.</param>
+		/// <param name="status">status to be updated.</param>
+		Task ChangeBankStatusAsync(Guid id, Status status);
 	}
 }
---
+++ Model/Status.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BankingRepository.Model
+{
+    /// <summary>
+    /// Represents status.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Status
+    {
+        /// <summary>
+        /// Created status.
+        /// </summary>
+        Created = 0,
+
+        /// <summary>
+        /// Created status.
+        /// </summary>
+        Active = 1,
+    }
+}
